5cb14f6fb653937c28683fdfd43ecc8b62dd0d0b
TonyHo <tonyho@via.com.tw>
2023-09-30 13:20:41 +0800

    update code

--- dsn/testbench/tb_fsic.v
+++ dsn/testbench/tb_fsic.v
@@ -24,8 +24,12 @@
 `define USER_PROJECT_SIDEBAND_SUPPORT 1
 
 module tb_fsic #( parameter BITS=32,
-		parameter pUSER_PROJECT_SIDEBAND_WIDTH   = 5,
-		parameter pSERIALIO_WIDTH   = 13,
+		`ifdef USER_PROJECT_SIDEBAND_SUPPORT
+			parameter pUSER_PROJECT_SIDEBAND_WIDTH   = 5,
+			parameter pSERIALIO_WIDTH   = 13,
+		`else
+			parameter pSERIALIO_WIDTH   = 12,
+		`endif
 		parameter pADDR_WIDTH   = 15,
 		parameter pDATA_WIDTH   = 32,
 		parameter IOCLK_Period	= 10,
